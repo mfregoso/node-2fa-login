fe1aab73e11929267594b2229613649ed8ffbce4
mfregoso <mfregoso@users.noreply.github.com>
2018-12-07 00:23:13 -0800

    notes

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,6 @@
 exports.register = (req, resp) => {
+  // email, password, name, phone
+  // redirect to page using SMS code to confirm registration
   let email = req.query.email;
   let password = req.query.password;
   if (email && password) {
@@ -6,6 +8,19 @@ exports.register = (req, resp) => {
   } else {
     resp.send("error!");
   }
+};
+
+exports.confirmRegistration = (req, resp) => {
+  // BONUS: enter code from phone, match against database to confirm account
+};
+
+exports.loginOne = (req, resp) => {
+  // validate user + pw combo + confirmed account, then create code & send SMS
+  // if NOT confirmed, send error
+};
+
+exports.loginTwo = (req, resp) => {
+  // user enters SMS code, checks against DB to complete signin
 };
 
 module.exports = exports;
